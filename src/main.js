b24c21f22f5ce71e8b110ae58635a202b84c3ed2
emberati <alex.shak55@gmail.com>
2022-07-22 17:03:57 +0300

    Project initialized

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import '@/assets/css/reset.css'
+import '@/assets/css/global.css'
+import '@/assets/css/fonts.css'
+
+const app = createApp(App)
+
+app.mount('#app')
